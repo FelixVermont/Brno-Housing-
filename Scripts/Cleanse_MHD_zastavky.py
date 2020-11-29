4998d25accc0041b255c03df8bbbcb2124f2a1f5
Tina Nguyen <felix@tinkasmek.local>
2020-11-29 12:31:44 +0100

    cleaning

--- Scripts/Cleanse_MHD_zastavky.py
+++ Scripts/Cleanse_MHD_zastavky.py
@@ -2,12 +2,12 @@ import pandas as pd
 
 zs = pd.read_csv('Doprava\MHD_zastavky.csv')
 
-zs = zs[['X','Y','stop_name','zone_id']]
+zs = zs[['X', 'Y', 'stop_name', 'zone_id']]
 
 zs1 = zs[zs.zone_id == 100]
 zs2 = zs[zs.zone_id == 101]
 
-zs_new = pd.concat([zs1,zs2], axis = 1)
-zs_new.to_csv('Doprava\Clean_MHD_zastavky.csv', index = False)
+zs_new = pd.concat([zs1, zs2], axis=1)
+zs_new.to_csv('Doprava\Clean_MHD_zastavky.csv', index=False)
 
 print(zs_new)
